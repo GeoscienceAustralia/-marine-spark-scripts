ee82a985d29a66ac576970f21c52590236dd4e04
Duncan Gray <Duncan.Gray@ga.gov.au>
2018-08-28 15:05:52 +1000

    fixes

--- hive_load.sql
+++ hive_load.sql
@@ -1,7 +1,4 @@
-
-CREATE EXTERNAL TABLE test(lat float, lon float, depth float, time String, proj\
-ect String, vessel String, line String, profile String, beam int, accuracy floa\
-t, status String)
+CREATE EXTERNAL TABLE test(lat float, lon float, depth float, time String, project String, vessel String, line String, profile String, beam int, accuracy float, status String)
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY ','
 LOCATION 's3a://test-bathymetry-point-data-small/'
