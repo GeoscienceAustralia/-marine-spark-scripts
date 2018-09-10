475b95fff9e9fc11d1523dc6447b5a6d02f2f13a
Duncan Gray <Duncan.gray@gma.gov.au>
2018-09-10 06:09:02 +0000

    fixing

--- spark_jars.sh
+++ spark_jars.sh
@@ -1,3 +1,4 @@
 # This needs to be sourced
+export VERSION=2.0.2
 export JARS=file:///opt/geomesa/dist/spark/geomesa-hbase-spark-runtime_2.11-${VERSION}.jar,file:///usr/lib/hbase/conf/hbase-site.xml
 echo $JARS
