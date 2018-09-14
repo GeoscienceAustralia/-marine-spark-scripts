76579e2135ba9d1b9802f0a43235b63628ba8be9
Duncan Gray <Duncan.gray@gma.gov.au>
2018-09-14 06:58:57 +0000

    bug fix

--- gdelt_ingest.sh
+++ gdelt_ingest.sh
@@ -1,6 +1,6 @@
 mkdir gdelt
 cd gdelt
-seq 6 -1 1 | xargs -n 1 -I{} sh -c "date -d'{} days ago' +%Y%m%d" | xargs -n 1 -I{} aws s3 cp  s3://gdelt-open-data/events/{}.export.csv .
+seq 7 -1 2 | xargs -n 1 -I{} sh -c "date -d'{} days ago' +%Y%m%d" | xargs -n 1 -I{} aws s3 cp  s3://gdelt-open-data/events/{}.export.csv .
 
 # you'll need to ensure the hbase-site.xml is provided on the classpath...by default it is picked up by the tools from standard locations
 geomesa-hbase ingest -c geomesa.gdelt -C gdelt -f gdelt -s gdelt \*.csv
