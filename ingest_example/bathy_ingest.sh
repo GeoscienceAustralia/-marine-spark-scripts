2d027ac2d0f6943dff6b2a349191f3522cdb7232
Duncan Gray <Duncan.Gray@ga.gov.au>
2018-09-20 09:28:15 +1000

    modifying for ingest

--- ingest_example/bathy_ingest.sh
+++ ingest_example/bathy_ingest.sh
@@ -3,7 +3,7 @@
 # https://community.hortonworks.com/articles/183050/ingest-geospatial-data-ais-data-in-csv-format-by-u.html
 
 geomesa-hbase ingest \
--c ais \
--s ais_data_conf.sft \
--C ais_data_feature_conf.convert \
-sample.csv
+-c test_bathy \
+-s bathy.sft \
+-C bathy_txt.convert \
+bath_small.txt
