74267d61df060890eaa85e8b64fcd9ec7d7716a9
Duncan Gray <Duncan.gray@gma.gov.au>
2018-09-21 05:12:15 +0000

    load a standard bathy file

--- ingest_bathy.sh
+++ ingest_bathy.sh
@@ -1,7 +1,7 @@
 
-aws s3 cp s3://test-bathymetry-point-data-small/bath_small.txt .
+aws s3 cp s3://test-bathymetry-point-data/GA-4452_2016-146_3560_20160525_030635_SolanderMay2016.txt .
 geomesa-hbase ingest \
 -c bathy \
 -s bathy.sft \
 -C bathy.convert \
-bath_small.txt
+GA-4452_2016-146_3560_20160525_030635_SolanderMay2016.txt
